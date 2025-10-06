50f690dd63f1859e84634bb615b80f165fec7d8c
Leslee7789 <lesliefleischer8@gmail.com>
2025-10-06 05:35:41 -0700

    add bottom navigation

---
+++ lib/pages/bottomnav.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Bottomnav extends StatelessWidget {
+  void Function(int)? onTap;
+   Bottomnav({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: BottomNavigationBar(
+        onTap:(value) =>  onTap!(value),
+        items:[
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: 'home'),
+           BottomNavigationBarItem(icon: Icon(Icons.home),label: 'home'),
+   
+          ],
+          ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,28 @@
+import 'package:ecommerce_app/pages/bottomnav.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+int selectedIndex = 0;
+
+void navigateBottomBar(int index){
+  setState(() {
+    selectedIndex =index;
+  });
+}
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: Bottomnav(
+        onTap:(index) => navigateBottomBar(index),
+      ),
+    );
   }
 }
